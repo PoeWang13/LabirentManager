75ae8bfe84434c193682b610568486484d851464
PoeWang13 <53164541+PoeWang13@users.noreply.github.com>
2021-11-12 15:53:56 +0300

    Labirent Odaları
    Labirent odalarını kurma hazırlanıyor

---
+++ Assets/Script/LabirentCell.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LabirentCell : MonoBehaviour
+{
+    public bool isRoom;
+    public bool isUsed;
+    public GameObject wallUp;
+    public GameObject wallDown;
+    public GameObject wallRight;
+    public GameObject wallLeft;
+}
